f23b02a217b65376e70dc4dde29c0fd3c743168e
mjswart <mjswart@gmail.com>
2018-08-31 11:14:52 -0400

    update all statistics, not just ones associated with indexes

--- msdb.dbo.cs_CUSTOM_D2L_StatsUpdate.sql
+++ msdb.dbo.cs_CUSTOM_D2L_StatsUpdate.sql
@@ -1,79 +1,64 @@
-CREATE PROCEDURE [dbo].[cs_CUSTOM_D2L_StatsUpdate]
-
-@DBName SYSNAME,
-@JobName SYSNAME
-
+CREATE PROCEDURE dbo.cs_CUSTOM_D2L_StatsUpdate
+  @DBName SYSNAME,
+  @JobName SYSNAME
 AS
 
 SET NOCOUNT ON
 
-------------------start making additional customizations here-------------------
+--Number of rows modified threshold to trigger stats update in combination with
+-- "@stats_age_threshold_days" of days since last update 
+DECLARE @stats_rows_changed_threshold INT;
+SELECT TOP 1 @stats_rows_changed_threshold = CAST(ConfigValue AS INT)
+  FROM msdb.dbo.D2LJobsConfigValues
+ WHERE DatabaseName IN (@DBName, 'DefaultValue')
+   AND JobName = @JobName 
+   AND ConfigName = 'stats_rows_changed_threshold'
+ ORDER BY CASE WHEN DatabaseName = @DBName THEN 0 ELSE 1 END ASC;
 
-DECLARE @stats_rows_changed_threshold INT	--Number of rows modified threshold to trigger stats update in combination with "@stats_age_threshold_days" of days since last update 
-DECLARE @default_stats_sample_size INT		--This is a default sample rate/percentage we use for any stats created on a table with more than @stats_rows_count_threshold " rows changed
-DECLARE @stats_age_threshold_days INT			--Number of days since last update to trigger stats update in combination with "@stats_rows_changed_threshold" rows changed
-DECLARE @stats_rows_count_threshold INT	--Number of rows to determine if  FULL SAMPLE scan should be used. Less than this value means FULL SAMPLE otherwise SAMPLE = @default_stats_sample_size
-DECLARE @stats_HistoryKeepThreshold INT		--Keep stats update history for this many days
+--This is a default sample rate/percentage we use for any stats created on a
+-- table with more than @stats_rows_count_threshold " rows changed
+DECLARE @default_stats_sample_size INT;
+SELECT TOP 1 @default_stats_sample_size = CAST(ConfigValue AS INT)
+  FROM msdb.dbo.D2LJobsConfigValues
+ WHERE DatabaseName IN (@DBName, 'DefaultValue')
+   AND JobName = @JobName 
+   AND ConfigName = 'default_stats_sample_size'
+ ORDER BY CASE WHEN DatabaseName = @DBName THEN 0 ELSE 1 END ASC;
 
-IF EXISTS (SELECT * FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'stats_rows_changed_threshold')
-BEGIN
-	SELECT @stats_rows_changed_threshold = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'stats_rows_changed_threshold'
-END
-ELSE
-BEGIN
-	SELECT @stats_rows_changed_threshold = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [JobName] = @JobName AND [ConfigName] = 'stats_rows_changed_threshold'
-END
------
-IF EXISTS (SELECT * FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'default_stats_sample_size')
-BEGIN
-	SELECT @default_stats_sample_size = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'default_stats_sample_size'
-END
-ELSE
-BEGIN
-	SELECT @default_stats_sample_size = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [JobName] = @JobName AND [ConfigName] = 'default_stats_sample_size'
-END
------
-IF EXISTS (SELECT * FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'stats_age_threshold_days')
-BEGIN
-	SELECT @stats_age_threshold_days = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'stats_age_threshold_days'
-END
-ELSE
-BEGIN
-	SELECT @stats_age_threshold_days = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [JobName] = @JobName AND [ConfigName] = 'stats_age_threshold_days'
-END
------
-IF EXISTS (SELECT * FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'stats_rows_count_threshold')
-BEGIN
-	SELECT @stats_rows_count_threshold = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'stats_rows_count_threshold'
-END
-ELSE
-BEGIN
-	SELECT @stats_rows_count_threshold = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [JobName] = @JobName AND [ConfigName] = 'stats_rows_count_threshold'
-END
------
-IF EXISTS (SELECT * FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'stats_HistoryKeepThreshold')
-BEGIN
-	SELECT @stats_HistoryKeepThreshold = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [DatabaseName] = @DBName AND [JobName] = @JobName AND [ConfigName] = 'stats_HistoryKeepThreshold'
-END
-ELSE
-BEGIN
-	SELECT @stats_HistoryKeepThreshold = CAST(ConfigValue AS INT) FROM [msdb].[dbo].[D2LJobsConfigValues] WHERE [JobName] = @JobName AND [ConfigName] = 'stats_HistoryKeepThreshold'
-END
------
+--Number of days since last update to trigger stats update in combination with
+-- @stats_rows_changed_threshold" rows changed
+DECLARE @stats_age_threshold_days INT;
+SELECT TOP 1 @stats_age_threshold_days = CAST(ConfigValue AS INT)
+  FROM msdb.dbo.D2LJobsConfigValues
+ WHERE DatabaseName IN (@DBName, 'DefaultValue')
+   AND JobName = @JobName 
+   AND ConfigName = 'stats_age_threshold_days'
+ ORDER BY CASE WHEN DatabaseName = @DBName THEN 0 ELSE 1 END ASC;
 
---PRINT @stats_rows_changed_threshold
---PRINT @default_stats_sample_size
---PRINT @stats_age_threshold_days
---PRINT @stats_rows_count_threshold
---PRINT @stats_HistoryKeepThreshold
+--Number of rows to determine if  FULL SAMPLE scan should be used. Less than
+-- this value means FULL SAMPLE otherwise SAMPLE = @default_stats_sample_size
+DECLARE @stats_rows_count_threshold INT;
+SELECT TOP 1 @stats_rows_count_threshold = CAST(ConfigValue AS INT)
+  FROM msdb.dbo.D2LJobsConfigValues
+ WHERE DatabaseName IN (@DBName, 'DefaultValue')
+   AND JobName = @JobName 
+   AND ConfigName = 'stats_rows_count_threshold'
+ ORDER BY CASE WHEN DatabaseName = @DBName THEN 0 ELSE 1 END ASC;
 
-------------------stop making customizations here---------------------
+--Keep stats update history for this many days
+DECLARE @stats_HistoryKeepThreshold INT;
+SELECT TOP 1 @stats_HistoryKeepThreshold = CAST(ConfigValue AS INT)
+  FROM msdb.dbo.D2LJobsConfigValues
+ WHERE DatabaseName IN (@DBName, 'DefaultValue')
+   AND JobName = @JobName 
+   AND ConfigName = 'stats_HistoryKeepThreshold'
+ ORDER BY CASE WHEN DatabaseName = @DBName THEN 0 ELSE 1 END ASC;
 
 DECLARE @CollectStatsCmd NVARCHAR(MAX)
 DECLARE @ErrorMessage NVARCHAR(MAX)
 DECLARE @CurrentUpdateStatsCmd NVARCHAR(MAX)
 
---Verify DB exists....
+-- Verify DB exists....
 IF NOT EXISTS(SELECT name FROM sys.databases WHERE name = @DBName)
 BEGIN
 	GOTO ERROR_EXIT_HERE
@@ -83,81 +68,81 @@ END
 ---- Save progress of the completed actions (to MSDN database)
 ------------------------------------------------------------------------------------
 
-
 IF (OBJECT_ID('msdb.dbo.D2LStatsUpdateHistory') IS NULL)
 BEGIN
 
 	CREATE TABLE msdb.dbo.D2LStatsUpdateHistory
 	( 
-		[DatabaseName] sysname, 
-		[DatabaseId] int, 
-		[ObjectId] int,
-		[TableName] sysname NULL,
-		[TableSchema] sysname NULL, 
-		[IndexName] sysname NULL, 
-		[StatsId] int, 
-		[StatsName] sysname,
-		[StatsLastUpdated] DATETIME, 
-		[RowsCount] bigint,
-		[RowsModifiedCount] bigint,
-		[RowsSampledCount] bigint,
-		[Action] nvarchar(max) NULL,
-		[TimeStarted] DATETIME NULL,
-		[TimeCompleted] DATETIME NULL
+		DatabaseName sysname, 
+		DatabaseId int, 
+		ObjectId int,
+		TableName sysname NULL,
+		TableSchema sysname NULL, 
+		StatsId int, 
+		StatsName sysname,
+		StatsLastUpdated DATETIME, 
+		RowsCount bigint,
+		RowsModifiedCount bigint,
+		RowsSampledCount bigint,
+		Action nvarchar(max) NULL,
+		TimeStarted DATETIME NULL,
+		TimeCompleted DATETIME NULL
 	) 
 END;
+
 --------------Maintenance-------------------------------------
 DELETE FROM msdb.dbo.D2LStatsUpdateHistory 
-WHERE DatabaseName = @DBName
-AND
-TimeCompleted < DATEADD(DD, -@stats_HistoryKeepThreshold, GETDATE())
+ WHERE DatabaseName = @DBName
+   AND TimeCompleted < DATEADD(DD, -@stats_HistoryKeepThreshold, GETDATE())
 --------------------------------------------------------------
 
 --------------------------------------------------------------
 ----Load D2LStatsUpdateHistory with Stats Metadata
 --------------------------------------------------------------
 
-SET @CollectStatsCmd = 'USE [' + @DBName + ']
+SET @CollectStatsCmd = 'USE ' + @DBName + ';
+INSERT msdb.dbo.D2LStatsUpdateHistory (
+       DatabaseName,
+       DatabaseId,
+       ObjectId,
+       TableName,
+       TableSchema,
+       StatsId,
+       StatsName,
+       StatsLastUpdated,
+       RowsCount,
+       RowsModifiedCount,
+       RowsSampledCount )
+SELECT @DBName,
+	   DB_ID(@DBName),
+	   s.object_id,
+       OBJECT_NAME(s.object_id),
+	   OBJECT_SCHEMA_NAME(s.object_id),
+	   s.stats_id,
+	   s.name,
+       sp.last_updated, 
+	   sp.rows,
+	   sp.modification_counter,
+	   sp.rows_sampled
+  FROM sys.stats s
+ CROSS APPLY sys.dm_db_stats_properties(s.object_id, s.stats_id) sp
+ WHERE OBJECTPROPERTY(s.object_id,''IsMSShipped'') = 0
+   AND sp.last_updated < DATEADD(DD, -@stats_age_threshold_days, GETDATE())
+   AND sp.modification_counter >= @stats_rows_changed_threshold
+   AND s.name not in (
+	     SELECT StatsName 
+	       FROM msdb.dbo.D2LStatsUpdateHistory
+	      WHERE TimeCompleted IS NULL
+	        AND DatabaseName = @DBName
+       )';
 
-INSERT msdb.dbo.D2LStatsUpdateHistory
-SELECT DISTINCT
-	DB_NAME() AS [Col1],
-	DB_ID() AS [Col2],
-	i.object_id,
-    t.name as tablename,
-	SCHEMA_NAME(t.schema_id) AS [Col5],
-    i.name as index_name,
-	sp.stats_id,
-	STS.name,
-    sp.last_updated,
-	sp.[rows],
-	sp.modification_counter,
-	sp.rows_sampled,
-	NULL AS [Col12],
-	NULL AS [Col13],
-	NULL AS [Col14]
-FROM sys.indexes i
-JOIN sys.tables t on t.object_id = i.object_id
-CROSS APPLY sys.dm_db_stats_properties(i.object_id, i.index_id) sp
-JOIN
-sys.stats STS
-on i.object_id = STS.object_id
-AND
-sp.stats_id = STS.stats_id
-WHERE sp.last_updated < ''' + CONVERT(NVARCHAR(MAX), (DATEADD(DD, -@stats_age_threshold_days, GETDATE())), 120) + '''
-AND
-sp.modification_counter >= ' + CAST(@stats_rows_changed_threshold AS NVARCHAR(MAX)) + '
-AND
-STS.name not in
-(
-	SELECT [StatsName] FROM msdb.dbo.D2LStatsUpdateHistory
-	WHERE [TimeCompleted] IS NULL
-	AND [DatabaseName] = '''+ @DBName + '''
-)
-'
-
---PRINT (@CollectStatsCmd)
-EXEC (@CollectStatsCmd)
+exec sp_executesql @stmt = @CollectStatsCmd, 
+                   @params = N'@DBName sysname, 
+				               @stats_age_threshold_days int, 
+							   @stats_rows_changed_threshold int',
+				   @DBName = @DBName,
+				   @stats_age_threshold_days = @stats_age_threshold_days,
+				   @stats_rows_changed_threshold = @stats_rows_changed_threshold;
 
 -----------------Process D2LStatsUpdateHistory Table------------------
 DECLARE @SchemaN SYSNAME
@@ -170,67 +155,75 @@ DECLARE @RowsC BIGINT
 DECLARE @TimeStartedTemp DATETIME
 
 DECLARE cStats CURSOR FOR
-SELECT [TableName],	[TableSchema], [ObjectId], [IndexName],	[StatsId], [StatsName], [RowsCount] 
-FROM msdb.dbo.D2LStatsUpdateHistory
-WHERE DatabaseName = @DBName
-AND
-TimeCompleted IS NULL
-Order BY [StatsLastUpdated] DESC
+	SELECT TableName,	
+	       TableSchema, 
+		   ObjectId, 
+		   IndexName,	
+		   StatsId, 
+		   StatsName, 
+		   RowsCount 
+      FROM msdb.dbo.D2LStatsUpdateHistory
+     WHERE DatabaseName = @DBName
+       AND TimeCompleted IS NULL
+  ORDER BY StatsLastUpdated DESC;
 
-OPEN cStats
+OPEN cStats;
 	
-FETCH NEXT FROM cStats INTO @TableN, @SchemaN, @ObjectId , @IndexN, @StatsId, @StatsN, @RowsC
+FETCH NEXT FROM cStats 
+	INTO @TableN, 
+		 @SchemaN, 
+		 @ObjectId , 
+		 @IndexN, 
+		 @StatsId, 
+		 @StatsN, 
+		 @RowsC;
 
-WHILE @@FETCH_STATUS = 0
+WHILE ( @@FETCH_STATUS = 0 )
 BEGIN
-
-
-	IF(@RowsC > @stats_rows_count_threshold)
-	BEGIN
-		SET @CurrentUpdateStatsCmd = 'USE [' + @DBName + ']
-
-		UPDATE STATISTICS [' + @SchemaN + '].[' + @TableN + '](' + @StatsN + ') WITH SAMPLE ' + CAST(@default_stats_sample_size AS NVARCHAR(MAX)) + ' PERCENT'
-
-	END
-	ELSE
-	BEGIN
-		SET @CurrentUpdateStatsCmd = 'USE [' + @DBName + ']
-
-		UPDATE STATISTICS [' + @SchemaN + '].[' + @TableN + '](' + @StatsN + ') WITH SAMPLE 100 PERCENT'
-	END
+  DECLARE @Percentage NVARCHAR(5);
+  SET @Percentage = CASE 
+                      WHEN @RowsC > @stats_rows_count_threshold 
+                           THEN CAST(@default_stats_sample_size AS NVARCHAR(5))
+                      ELSE CAST(100 AS NVARCHAR(5))
+                    END;
+  SET @CurrentUpdateStatsCmd = 'USE ' + @DBName + ';
+    UPDATE STATISTICS ' + QUOTENAME(@SchemaN) + '.' + QUOTENAME(@TableN) + '(' + QUOTENAME(@StatsN) + ') 
+      WITH SAMPLE ' + @Percentage + ' PERCENT;';
 
 	--Update the start time
-
-	SET @TimeStartedTemp = GETDATE();
-
 	UPDATE msdb.dbo.D2LStatsUpdateHistory
-	SET [TimeStarted] = @TimeStartedTemp, [Action] = @CurrentUpdateStatsCmd
-	WHERE [TableSchema] = @SchemaN
-	AND [TableName] = @TableN
-	AND [StatsName] = @StatsN
-	AND [DatabaseName] = @DBName
-	AND [ObjectId] = @ObjectId;
+	   SET TimeStarted = GETDATE(), 
+	       Action = @CurrentUpdateStatsCmd
+	 WHERE TableSchema = @SchemaN
+	   AND TableName = @TableN
+	   AND StatsName = @StatsN
+	   AND DatabaseName = @DBName
+	   AND ObjectId = @ObjectId;
 
-	--PRINT(@CurrentUpdateStatsCmd);
-	EXEC(@CurrentUpdateStatsCmd);
+	EXEC sp_executesql @CurrentUpdateStatsCmd;
 
 	--Update the time completed
-
 	UPDATE msdb.dbo.D2LStatsUpdateHistory
-	SET TimeCompleted = GETDATE()
-	WHERE [TableSchema] = @SchemaN
-	AND [TableName] = @TableN
-	AND [StatsName] = @StatsN
-	AND [DatabaseName] = @DBName
-	AND [ObjectId] = @ObjectId
-	AND [TimeStarted] = @TimeStartedTemp;
+	   SET TimeCompleted = GETDATE()
+	 WHERE TableSchema = @SchemaN
+	   AND TableName = @TableN
+	   AND StatsName = @StatsN
+	   AND DatabaseName = @DBName
+	   AND ObjectId = @ObjectId
+	   AND TimeStarted = @TimeStartedTemp;
 
-	FETCH NEXT FROM cStats INTO @TableN, @SchemaN, @ObjectId , @IndexN, @StatsId, @StatsN, @RowsC;
+	FETCH NEXT FROM cStats 
+	INTO @TableN, 
+		 @SchemaN, 
+		 @ObjectId , 
+		 @IndexN, 
+		 @StatsId, 
+		 @StatsN, 
+		 @RowsC;
 END
 		
 CLOSE cStats
 DEALLOCATE cStats
-
 
 GOTO SUCCESS_EXIT_HERE
 
